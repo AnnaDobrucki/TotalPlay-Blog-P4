b81ad571743d0dbeeebfe0b1db8ddb480eb99398
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2023-11-02 09:14:41 +0000

    add Read.Me and Testing.md files

--- TotalBlog/views.py
+++ TotalBlog/views.py
@@ -124,8 +124,7 @@ def edit_booking_view(request, booking_id):
                 form = OnlineForm(data=request.POST, instance=booking)
                 if form.is_valid():
                     form.save()
-                    # Pops up a message to the user when a booking is edited
-                    messages.success(request, 'Your booking has been updated')
+                    messages.success(request, 'Booking has been updated')
                     return redirect('/')
         else:
             messages.error(request, 'You do not have the authority to access this page!')
---
+++ documentation/testing.md
@@ -0,0 +1,8 @@
+# Testing
+
+## Index
+
+ * [Error resolution and Debugging](#error-resolution-and-debugging)
+ * [Manual testing](#manual-testing)
+ * [Code Institue Linter testing](#code-institue-linter-testing)
+ * [Lighthouse Testing](#lighthouse-testing)
